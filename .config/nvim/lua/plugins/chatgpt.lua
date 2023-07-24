7d24ec10aa63d75d206ba8beb13df7ac26688786
michaelbahchevanov <bahchevanov.mihail@gmail.com>
2023-07-24 15:42:38 +0200

    cfg

---
+++ .config/nvim/lua/plugins/chatgpt.lua
@@ -0,0 +1,17 @@
+plugins = {
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("chatgpt").setup {
+        api_key_cmd = "pass show openai",
+      }
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+  },
+}
+return plugins
